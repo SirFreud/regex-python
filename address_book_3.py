e44da74b5552cf4abad94fbeabd597cfb8e93a00
SirFreud <mikevaldez92@gmail.com>
2016-08-21 22:32:54 -0500

    Used .finditer to print all names and emails nicely

--- address_book_3.py
+++ address_book_3.py
@@ -5,13 +5,17 @@ names_file = open('names.txt', encoding='utf-8')
 data = names_file.read()
 names_file.close()
 
-line = re.search(r'''
-    ^(?P<name>[-\w ]*,\s[-\w ]+)\t # Find LastName, FirstName
+line = re.compile(r'''
+    ^(?P<name>(?P<last>[-\w ]*),\s(?P<first>[-\w ]+))\t # Find LastName, FirstName
     (?P<email>[-\w\d.+]+@[-\w\d.]+)\t # Email
     (?P<phone>\(?\d{3}\)?-?\s?\d{3}-\d{4})?\t # Phone number
     (?P<job>[\w\s]+,\s[\w\s.]+)\t? # Job & company
     (?P<twitter>@[\w\d]+)?$ # Twitter
-''', data, re.X | re.MULTILINE) # Can also do re.M
+''', re.X | re.MULTILINE)  # Can also do re.M
 
-print(line)
-print(line.groupdict())
+# print(line)
+print(re.search(line, data).groupdict())
+print(line.search(data).groupdict())
+
+for match in line.finditer(data):
+    print('{first} {last} <{email}>'.format(**match.groupdict()))

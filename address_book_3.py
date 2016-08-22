bce733733d434207adab4addf8e2e9615fd58721
SirFreud <mikevaldez92@gmail.com>
2016-08-21 21:56:15 -0500

    Made several fields, including LastName and Email optional

--- address_book_3.py
+++ address_book_3.py
@@ -6,9 +6,9 @@ data = names_file.read()
 names_file.close()
 
 print(re.findall(r'''
-    ([-\w ]+,\s[-\w ]+)\t # Find LastName, FirstName
+    ^([-\w ]*,\s[-\w ]+)\t # Find LastName, FirstName
     ([-\w\d.+]+@[-\w\d.]+)\t # Email
-    (\(?\d{3}\)?-?\s?\d{3}-\d{4})\t # Phone number
-    ([\w\s]+,\s[\w\s]+)\t # Job & company
-    (@[\w\d]+) # Twitter
-''', data, re.X))
+    (\(?\d{3}\)?-?\s?\d{3}-\d{4})?\t # Phone number
+    ([\w\s]+,\s[\w\s.]+)\t? # Job & company
+    (@[\w\d]+)?$ # Twitter
+''', data, re.X | re.MULTILINE)) # Can also do re.M

420ace2d8807baced9a1fc286c4cf3964ab3ba81
SirFreud <mikevaldez92@gmail.com>
2016-08-21 21:59:55 -0500

    Now have the entire regex mapped to values and returning a dictionary

--- address_book_3.py
+++ address_book_3.py
@@ -5,10 +5,13 @@ names_file = open('names.txt', encoding='utf-8')
 data = names_file.read()
 names_file.close()
 
-print(re.findall(r'''
-    ^([-\w ]*,\s[-\w ]+)\t # Find LastName, FirstName
-    ([-\w\d.+]+@[-\w\d.]+)\t # Email
-    (\(?\d{3}\)?-?\s?\d{3}-\d{4})?\t # Phone number
-    ([\w\s]+,\s[\w\s.]+)\t? # Job & company
-    (@[\w\d]+)?$ # Twitter
-''', data, re.X | re.MULTILINE)) # Can also do re.M
+line = re.search(r'''
+    ^(?P<name>[-\w ]*,\s[-\w ]+)\t # Find LastName, FirstName
+    (?P<email>[-\w\d.+]+@[-\w\d.]+)\t # Email
+    (?P<phone>\(?\d{3}\)?-?\s?\d{3}-\d{4})?\t # Phone number
+    (?P<job>[\w\s]+,\s[\w\s.]+)\t? # Job & company
+    (?P<twitter>@[\w\d]+)?$ # Twitter
+''', data, re.X | re.MULTILINE) # Can also do re.M
+
+print(line)
+print(line.groupdict())
